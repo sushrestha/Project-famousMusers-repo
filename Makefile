39f9257a47cc2021c88524baf70a4b0087755af8
Andrew Tackett <andrew.tackett@gmail.com>
2015-04-03 18:34:09 -0500

    get auth_skeleton from Dr. F"

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+s:
+	rails s -b 0.0.0.0
+
+m:
+	rake db:migrate:reset db:seed
+	annotate
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:show]
+  before_action :correct_user,   only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, 
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+end
