9faad7aa028b4a94583a24993494398703dc9182
sushrestha <sujit.shrestha421@gmail.com>
2015-04-24 21:30:55 +0000

    made accessed only if logged in

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,5 @@
 class CompetitionsController < ApplicationController
-  #before_action :logged_in_muser, :only => [:new, :create, :edit, :update, :destroy, :show]
+  before_action :logged_in_muser
   #before_action :correct_muserid,   only: [:edit, :update, :destroy]
   #before_filter :check_for_cancel, :only => [:create, :update]
   #before_filter :find_musing, :only => [:show, :edit, :update,  :destroy]
