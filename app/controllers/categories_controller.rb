d53d9a67b541ddd5dea1a42307ce9fb5ee57294c
sushrestha <sujit.shrestha421@gmail.com>
2015-04-24 21:45:54 +0000

    updated show category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,6 +12,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @musings = Musing.where(category_id: @category.id)
   end
 
   # GET /categories/new
