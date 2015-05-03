1e392cd66a16b5ac692491a14f542e2d84714aa9
sushrestha <sujit.shrestha421@gmail.com>
2015-05-03 06:31:54 +0000

    pulled

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id         :integer          not null, primary key
+#  linktype   :string
+#  linkid     :integer
+#  unread     :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  muser_id   :integer
+#
+
 class Notification < ActiveRecord::Base
   belongs_to :recipient, foreign_key: 'muser_id'
    
