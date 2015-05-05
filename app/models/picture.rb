7a398b52217177fcdb5d13e121fd55b1983438ae
sushrestha <sujit.shrestha421@gmail.com>
2015-05-05 21:10:47 +0000

    annotate

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: pictures
+#
+#  id           :integer          not null, primary key
+#  comment      :string
+#  name         :string
+#  content_type :string
+#  data         :binary
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Picture < ActiveRecord::Base
 	validates :content_type, presence: true
     #with: /^image/,
