e89e2e8f9059077e0d028e2eafa2205a7b6463cd
sushrestha <sujit.shrestha421@gmail.com>
2015-05-05 06:01:08 +0000

    annotated

--- app/models/competition_rating.rb
+++ app/models/competition_rating.rb
@@ -8,6 +8,7 @@
 #  updated_at       :datetime         not null
 #  competition_id   :integer
 #  musing_id        :integer
+#  muser_id         :integer
 #
 
 class CompetitionRating < ActiveRecord::Base
