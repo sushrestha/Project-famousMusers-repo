ef62220f9d14a86e66394afde9434d3772416e23
sushrestha <sujit.shrestha421@gmail.com>
2015-05-06 00:08:56 +0000

    commented all test code in this file

--- test/controllers/flagged_musings_controller_test.rb
+++ test/controllers/flagged_musings_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class FlaggedMusingsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+	
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
 
 end
--- test/controllers/musings_controller_test.rb
+++ test/controllers/musings_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class MusingsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+
+def setup
+	
+end
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
 
 end
